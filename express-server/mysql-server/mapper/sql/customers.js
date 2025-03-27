a769c2dfcc12ae3d0f8daed7cb99812fdd8015c5
Lee min young <lmy1147@naver.com>
2025-03-27 17:52:18 +0900

    0327

---
+++ express-server/mysql-server/mapper/sql/customers.js
@@ -0,0 +1,17 @@
+// mapper/sql/customers.js
+// Table : customers;
+
+const selectAll = 
+`SELECT id
+		,name
+		,email
+    ,phone
+    ,address
+FROM customers
+ORDER BY id`;
+
+
+module.exports = {
+  selectAll,
+  // 'selectAll' : selectAll
+}
